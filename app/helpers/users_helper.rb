11c4ce2089a6eaeaf3b05077484174bbfbc43406
Adam Nickle <anickle060193@gmail.com>
2017-02-19 06:18:38 +0000

    Added user signup.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def gravatar_url( user, size: 80 )
+    id = Digest::MD5::hexdigest( user.email.downcase )
+    return "https://secure.gravatar.com/avatar/#{id}?s=#{size}"
+  end
+
 end

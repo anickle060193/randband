c2bb8e0c5e2c534f0993ee799de9950f16e657ff
Adam Nickle <anickle060193@gmail.com>
2017-02-19 00:26:54 +0000

    Abstracted page title creation.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,11 +10,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Choose-a-Band"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Choose-a-Band"
   end
 
 end

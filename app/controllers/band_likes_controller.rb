077461334029e976d9cae44d2070036317e5df2e
Adam Nickle <anickle060193@gmail.com>
2017-03-25 19:12:35 +0000

    Added ajax support to band like/unlike form.

--- app/controllers/band_likes_controller.rb
+++ app/controllers/band_likes_controller.rb
@@ -2,19 +2,23 @@ class BandLikesController < ApplicationController
   before_action :logged_in_user, only: [ :create, :destroy ]
 
   def create
-    band = Band.find_by( id: params[ :band_id ] )
-    band ||= Band.find_by_provider( params[ :provider ], params[ :provider_id ] )
-    current_user.like( band ) unless current_user.likes?( band )
-    redirect_to band_link( band )
+    @band = Band.find_by( id: params[ :band_id ] )
+    @band ||= Band.find_by_provider( params[ :provider ], params[ :provider_id ] )
+    current_user.like( @band ) unless current_user.likes?( @band )
+
+    respond_to do |format|
+      format.html { redirect_to band_link( @band ) }
+      format.js
+    end
   end
 
   def destroy
-    band_like = BandLike.find_by( id: params[ :id ] )
-    if band_like.nil?
-      redirect_to band_link( Band.find_by( id: params[ :band_id ] ) )
-    else
-      current_user.unlike( band_like.band )
-      redirect_to band_link( band_like.band )
+    @band = BandLike.find_by( id: params[ :id ] )&.band || Band.find_by( id: params[ :band_id ] )
+    current_user.unlike( @band ) unless !current_user.likes?( @band )
+
+    respond_to do |format|
+      format.html { redirect_to band_link( @band ) }
+      format.js
     end
   end
 
